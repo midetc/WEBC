c31bdce22d2a81cb9fbe858b12edcee1e8485e20
Your Name <example@example.com>
2025-05-16 13:18:14 +0200

    Очищення проекту та оптимізація структури

--- backend/run.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from app.db.init_db import init_db
-
-if __name__ == "__main__":
-    # Ініціалізуємо базу даних
-    init_db()
-    
-    # Запускаємо Fast API сервер
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True) 
